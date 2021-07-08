8f51b9a52855d618a8e9b22d695ff060f67c18f2
Anastasiia Bateman <Anastasiia.bateman@gmail.com>
2021-07-08 16:51:49 -0400

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :shows #created by scaffold 
   #get 'home/index'
   get 'home/about'
-  root 'home#index'
+  #root 'home#index'
+  root 'shows#index'
 end
